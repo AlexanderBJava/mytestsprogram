0e0d2a8cd62e696194fe493d3b679843ad31b783
AlexanderB <AlexanderBJava@gmail.com>
2018-09-19 23:53:47 +0300

    no message

--- src/main/java/robotNoSpring/Robot.java
+++ src/main/java/robotNoSpring/Robot.java
@@ -1,42 +1,17 @@
 package robotNoSpring;
 
+import lombok.Data;
 import robotNoSpring.schemes.Hand;
 import robotNoSpring.schemes.Head;
 import robotNoSpring.schemes.Leg;
 
-
+@Data
 public class Robot {
     private Head head;
     private Hand hand;
     private Leg leg;
 
-    public Head getHead() {
-        return head;
-    }
 
-    public void setHead(Head head) {
-        this.head = head;
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
-
-    public void setHand(Hand hand) {
-        this.hand = hand;
-    }
-
-    public Leg getLeg() {
-        return leg;
-    }
-
-    public void setLeg(Leg leg) {
-        this.leg = leg;
-    }
-
-    public Robot() {
-
-    }
 
     public Robot(Head head, Hand hand, Leg leg) {
         this.head = head;
